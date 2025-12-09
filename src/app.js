4f717a1e78adfd49e7fc13aaa726510d2c3038b2
StivGav <brandonbsgr@gmail.com>
2025-12-09 20:02:49 +0000

    Generador de Excusas simple

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  
+
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+    const r = arr => arr[Math.floor(Math.random() * arr.length)];
+    document.getElementById('excuse').innerHTML = `${r(who)} ${r(action)} ${r(what)} ${r(when)}`;
+  
 };
